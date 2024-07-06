8c49444efcf7a4c7ee4130e8ec21ed1836ffd09d
VroomPsshh <siddhantgauttam@gmail.com>
2024-07-06 22:04:44 +0530

    setting LongClickListener event handler for recyclerView items

--- app/src/main/java/com/vroom_psshh/notes20/UserInputAdapter.kt
+++ app/src/main/java/com/vroom_psshh/notes20/UserInputAdapter.kt
@@ -1,5 +1,6 @@
 package com.vroom_psshh.notes20
 
+import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -8,7 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.vroom_psshh.notes20.databinding.NotesEntryListBinding
 import com.vroom_psshh.notes20.roomDB.UserInput
 
-class UserInputAdapter(private val context: Context, private var entryList :List<UserInput>): RecyclerView.Adapter<UserInputAdapter.ViewHolder>() {
+class UserInputAdapter(private val context: Context, private var entryList: List<UserInput>) :
+    RecyclerView.Adapter<UserInputAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = NotesEntryListBinding.inflate(LayoutInflater.from(context), parent, false)
@@ -24,19 +26,29 @@ class UserInputAdapter(private val context: Context, private var entryList :List
         return entryList.size
     }
 
-    fun updateNotes(notes: List<UserInput>){
+    fun updateNotes(notes: List<UserInput>) {
         this.entryList = notes
         notifyDataSetChanged()
     }
 
-    class ViewHolder(private var binding: NotesEntryListBinding): RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private var binding: NotesEntryListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(userInput: UserInput){
+        fun bind(userInput: UserInput) {
             val context = binding.root.context
             binding.userInput = userInput
             binding.executePendingBindings()
-            binding.notesCard.setOnClickListener{
-                Toast.makeText(context, "hey this is your current position $adapterPosition", Toast.LENGTH_SHORT).show()
+            val dialog = Dialog(context)
+            dialog.setContentView(R.layout.custom_dialog_layout)
+            binding.notesCard.setOnLongClickListener {
+                Toast.makeText(
+                    context,
+                    "hey this is your current position $adapterPosition",
+                    Toast.LENGTH_SHORT
+                ).show()
+                dialog.show()
+                true
+                // Return 'true' to indicate that the event is consumed
             }
         }
     }
